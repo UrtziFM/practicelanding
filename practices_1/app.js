def6f54efcd6c77014dda2657b94c8eafd5494a4
UrtziFM <urtzi.arana13@gmail.com>
2020-10-19 20:42:02 +0200

    second exercise

--- practices_1/app.js
+++ practices_1/app.js
@@ -1,3 +1,8 @@
+let age = document.getElementById('age').value
 function validation() {
-    if ()
+    if (age > 17) {
+        return document.write( "You can Drive" )
+    } else {
+        return document.write ( "You are so young to drive" )
+    }
 }
