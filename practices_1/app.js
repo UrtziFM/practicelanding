3650747f86a40632992f0e1a29657c7d33c135a4
UrtziFM <urtzi.arana13@gmail.com>
2020-10-20 23:09:50 +0200

    second exercise

--- practices_1/app.js
+++ practices_1/app.js
@@ -1,3 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("form").addEventListener('submit', validation); 
+  });
+
 let age = document.getElementById('age').value
 function validation() {
     if (age > 17) {
@@ -6,3 +10,34 @@ function validation() {
         return document.write ( "You are so young to drive" )
     }
 }
+
+let result = document.getElementById('result').value
+console.log(result)
+
+function validation_exam() {
+    if (result > 0 && result <= 10) {
+        if (result < 3) {
+            alert("Muy deficiente");
+        }
+        else if (result < 5) {
+            alert("Insuficiete");
+        }
+        else if (result < 6) {
+            alert("Suficiente");
+        }
+        else if (result < 7) {
+            alert("Bien");
+        }
+        else if (result < 9) {
+            alert("Notable"); 5
+        }
+        else if (result >= 9) {
+            alert("Sobresaliente");
+        }
+    }
+    //SI LA NOTA NO ES ENTRE 0 Y 10
+    else {
+        alert("Nota erronea");
+    }
+}
+
